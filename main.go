093ef94e9182d26eaef311aaaf8371d56cc6acf8
mavenraven <me@mavenraven.org>
2023-02-19 22:33:34 -0500

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"flag"
 	"fmt"
+	"io"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -12,13 +15,15 @@ import (
 )
 
 func main() {
-	port := flag.Int("port", 22, "port number that sshd is listening on")
+	port := flag.Int("ssh-port", 22, "port number that sshd is listening on")
 	host := flag.String("host", "", "name of host / ip address that sshd is listening on")
 
 	home := os.Getenv("HOME")
 
 	privateKeyPath := flag.String("private-key-path", "", "location of private key used to login, $HOME/.ssh/id_rsa will be used if not set")
-	tcpOpenPortList := flag.String("tcp-open-ports", "", "tcp ports to open on the remote machine, comma seperated")
+	tcpOpenPortList := flag.String("tcp-server-open-ports", "", "tcp ports to open on the remote machine, comma seperated")
+	_ = flag.String("server-listen", "", "tcp ports to open on the remote machine, comma seperated")
+	user := flag.String("user", "root", "name of user to use on remote machine")
 
 	flag.Parse()
 
@@ -45,26 +50,26 @@ func main() {
 	var err error
 
 	fmt.Println("attempting to connect as root...")
-	client, err = simplessh.ConnectWithKeyFile(socket, "root", *privateKeyPath)
+	client, err = simplessh.ConnectWithKeyFile(socket, *user, *privateKeyPath)
 	assertNoErr(err, "unable to establish a connection")
 	defer client.Close()
 	fmt.Println("connection to remote host established!")
 
 	fmt.Println("clearing existing firewall rules...")
 	//ran into this: https://askubuntu.com/a/1293947
-	sshCommand(client, "sudo iptables -P INPUT ACCEPT")
-	sshCommand(client, "sudo iptables -P OUTPUT ACCEPT")
-	sshCommand(client, "sudo iptables -F")
+	sshCommand(client, "iptables -P INPUT ACCEPT")
+	sshCommand(client, "iptables -P OUTPUT ACCEPT")
+	sshCommand(client, "iptables -F")
 	fmt.Println("all existing firewall rules removed")
 
 	fmt.Println("adding rule to allow ssh...")
-	sshCommand(client, "sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT")
+	sshCommand(client, "iptables -A INPUT -p tcp --dport ssh -j ACCEPT")
 	fmt.Println("rule to allow ssh added")
 
 	for _, port := range tcpPortsToOpen {
 		fmt.Printf("adding rule to open tcp port %v\n", port)
 
-		cmd := fmt.Sprintf("sudo iptables -A INPUT -p tcp --dport %v -j ACCEPT", port)
+		cmd := fmt.Sprintf("iptables -A INPUT -p tcp --dport %v -j ACCEPT", port)
 		sshCommand(client, cmd)
 
 		fmt.Printf("rule to open tcp port %v added\n", port)
@@ -72,16 +77,83 @@ func main() {
 
 	fmt.Println("adding rule to deny all other incoming tcp traffic...")
 
-	cmd := "sudo iptables -L | grep 'ACCEPT' | grep 'ssh' > /dev/null && sudo iptables -P INPUT DROP"
+	//a bit of extra safety to ensure the ssh rule is there
+	cmd := "iptables -L | grep 'ACCEPT' | grep 'ssh' > /dev/null && sudo iptables -P INPUT DROP"
 	sshCommand(client, cmd)
 
-	fmt.Println("rule to deny all other traffic ended")
+	fmt.Println("rule to deny all other traffic added")
+
+	fmt.Println("installing podman...")
+	sshCommand(client, "apt install podman -y")
+	fmt.Println("podman installed")
+
+	fmt.Println("creating tarball...")
+	tarballName := createTarball()
+	if tarballName == "" {
+		fmt.Printf("no files specificed for uploading, nothing left to do")
+		os.Exit(0)
+	}
+	fmt.Println("tarball created")
 
 	return
 }
 
+func createTarball() string {
+	if len(flag.Args()) == 0 {
+		return ""
+	}
+
+	tarballFile, err := os.CreateTemp("", "")
+	assertNoErr(err, "unable to create tarball file")
+
+	fmt.Printf("creating tarball of files to upload: %v...\n", tarballFile.Name())
+	defer tarballFile.Close()
+
+	gzipWriter := gzip.NewWriter(tarballFile)
+	defer gzipWriter.Close()
+
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer tarWriter.Close()
+
+	for _, filePath := range flag.Args() {
+		func() {
+			fileToAdd, err := os.Open(filePath)
+			assertNoErr(err, "could not open file to add to tarball: %v", filePath)
+			defer fileToAdd.Close()
+
+			stat, err := fileToAdd.Stat()
+			assertNoErr(err, "could not get stat of file to add to tarball: %v", filePath)
+
+			header := &tar.Header{
+				Name:    filePath,
+				Size:    stat.Size(),
+				Mode:    int64(stat.Mode()),
+				ModTime: stat.ModTime(),
+			}
+
+			err = tarWriter.WriteHeader(header)
+			assertNoErr(err, "could not write header for tarball: %v", filePath)
+
+			_, err = io.Copy(tarWriter, fileToAdd)
+			assertNoErr(err, "could not copy file to tarball: %v", filePath)
+		}()
+	}
+
+	return tarballFile.Name()
+}
+
 func sshCommand(client *simplessh.Client, command string) {
-	output, err := client.Exec(command)
+	sudoPassword := os.Getenv("GODEPLOY_SUDO")
+
+	var output []byte
+	var err error
+
+	if sudoPassword == "" {
+		output, err = client.Exec(command)
+	} else {
+		output, err = client.ExecSudo(sudoPassword, command)
+	}
+
 	if err != nil {
 		fmt.Printf("output of failed command: %v", string(output))
 		fmt.Printf("error: %v\n", err)
@@ -90,9 +162,27 @@ func sshCommand(client *simplessh.Client, command string) {
 
 }
 
-func assertNoErr(err error, message string) {
+func assertNoErr(err error, message ...string) {
 	if err != nil {
-		fmt.Println(message)
+		if len(message) == 0 {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+
+		if len(message) == 1 {
+			fmt.Println(message[0])
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+
+		rest := make([]interface{}, 0, len(message))
+		for _, m := range message {
+			rest = append(rest, m)
+		}
+
+		rest = append(rest, "\n")
+
+		fmt.Printf(message[0], rest...)
 		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
