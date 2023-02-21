aed7b46c0881ef31386aa9a52332472bb5ea6a32
mavenraven <me@mavenraven.org>
2023-02-21 00:32:55 -0500

    --wip-- [skip ci]

--- cmd/setup.go
+++ cmd/setup.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sfreiberg/simplessh"
 	"github.com/spf13/cobra"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -35,10 +36,22 @@ func setup(cmd *cobra.Command, args []string) {
 	})
 	defer client.Close()
 
-	step(&counter, "checking version of server", func() {
-		_, err := client.Exec("uname -a | grep 'Linux Ubuntu-2204-jammy-amd64-base'")
-		if err != nil {
+	step(&counter, "checking OS version of server", func() {
+		output, err := client.Exec("uname -a")
+		assertNoErr(err, "could not get os version")
+
+		if !strings.Contains(string(output), "Linux Ubuntu-2204-jammy-amd64-base") {
 			color.Red("snakeplant is only supported for https://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/")
+			os.Exit(1)
+		}
+	})
+
+	step(&counter, "checking architecture of server", func() {
+		output, err := client.Exec("uname -p")
+		assertNoErr(err, "could not get architecture")
+
+		if !strings.Contains(string(output), "x86_64") {
+			color.Red("snakeplant is only supported on x86_64")
 			os.Exit(1)
 		}
 	})
@@ -58,6 +71,25 @@ func setup(cmd *cobra.Command, args []string) {
 		sshCommand(client, "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections")
 		sshCommand(client, "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections")
 		sshCommand(client, "apt-get install iptables-persistent -y")
+	})
+
+	step(&counter, "installing pack", func() {
+		fileName := "pack-v0.28.0-linux-arm64.tgz"
+
+		sshCommand(client, "curl -m 5 -O -L https://github.com/buildpacks/pack/releases/download/v0.28.0/pack-v0.28.0-linux-arm64.tgz")
+
+		out, err := client.Exec(fmt.Sprintf("sha256sum %v | awk '{print $1}'", "pack-v0.28.0-linux-arm64.tgz"))
+		assertNoErr(err, "could not get hash of pack-cli tarball")
+		fmt.Printf(string(out))
+
+		if strings.TrimSpace(string(out)) != "f4940962d1d65b3abcb1996e98cae6497f525999991e9d9dbc7d78a4029d5bb6" {
+			fmt.Println("pack-cli tarball corrupt, or someone is doing something sneaky...")
+			os.Exit(1)
+		}
+
+		sshCommand(client, fmt.Sprintf("tar xvf %v", fileName))
+		sshCommand(client, "mv pack /usr/local/bin/pack")
+		sshCommand(client, "chmod +x /usr/local/bin/pack")
 	})
 
 }
